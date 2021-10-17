cd89106ddeff8cb0fcef446655a84d956fc587ae
Rodrigo Couto <r@rodg.co>
2021-10-17 19:37:15 -0300

    Update tools

--- web3/contracts/MyEpicNFT.sol
+++ web3/contracts/MyEpicNFT.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.0;
 
 // We need some util functions for strings.
@@ -14,7 +16,7 @@ contract MyEpicNFT is ERC721URIStorage {
 
 	// This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
 	// So, we make a baseSvg variable here that all our NFTs can use.
-	string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px;  }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+	string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
 	// I create three arrays, each with their own theme of random words.
 	// Pick some random funny words, names of anime characters, foods you like, whatever! 
