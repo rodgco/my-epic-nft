588132796d2dbfba82783e564bb6813f589ea451
Rodrigo Couto <r@rodg.co>
2021-10-17 21:36:58 -0300

    Trying to fix build automation - hardhat.config env 3

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,9 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
 	const accounts = await hre.ethers.getSigners();
 
 	for (const account of accounts) {
@@ -14,15 +14,19 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+let networks = {};
+
+if (process.env.PRIVATE_KEY) {
+	networks.rinkeby = {
+		url: process.env.STAGING_ALCHEMY_KEY || '',
+		accounts: [process.env.PRIVATE_KEY || '']
+	};
+}
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-	solidity: "0.8.0",
-	networks: {
-		rinkeby: {
-			url: process.env.STAGING_ALCHEMY_KEY || "",
-			accounts: [process.env.PRIVATE_KEY || ""],
-		},
-	},
+	solidity: '0.8.0',
+	networks
 };
