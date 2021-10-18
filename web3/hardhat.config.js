7c3f454d3d610438ee2669b914dac7dbb1d3ea8a
Rodrigo Couto <r@rodg.co>
2021-10-17 21:02:23 -0300

    Trying to fix build automation - hardhat.config env

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
 	solidity: "0.8.0",
 	networks: {
 		rinkeby: {
-			url: process.env.STAGING_ALCHEMY_KEY,
+			url: process.env.STAGING_ALCHEMY_KEY || "",
 			accounts: [process.env.PRIVATE_KEY],
 		},
 	},
